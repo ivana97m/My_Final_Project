12926b25634867a820ec8985a5bf85934ce47cac
ivana97m <ivsmilojevic@gmail.com>
2026-03-23 22:43:02 +0100

    session added

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from app.schemas.user import TokenResponse, UserLoginRequest, UserRegisterRequest, UserResponse
+
 router = APIRouter(tags=["auth"])
 
-@router.post("/auth", response_model=schemas.Token)
+@router.post("/auth", response_model=UserResponse,status_code=201, summary="Register new user")
 async def auth(request: Request):
 
