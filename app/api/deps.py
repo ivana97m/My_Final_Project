ef6074c44cf5ac2b69303ba03859a8ac68c55663
ivana97m <ivsmilojevic@gmail.com>
2026-03-28 12:13:23 +0100

    updated deps

---
+++ app/api/deps.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.exceptions import UnauthorizedException
+from app.core.security import decode_access_token
+from app.db.session import get_db
+from app.models.user import User
+from app.services.auth_service import auth_service
+
+
+security = HTTPBearer()
+
+async def get_current_user (
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
+    db: Annotated[AsyncSession, Depends(get_db)]
+)-> User:
+    payload = decode_access_token(credentials.credentials)
+    if not payload or "sub" not in payload:
+        raise UnauthorizedException("Invalid or expired token")
+
+    user_id = int(payload["sub"])
+    user = await auth_service.get_user_by_id(db, user_id)
+    return user
+
+CurrentUser=Annotated[User,Depends(get_current_user)]
+DB=Annotated[AsyncSession,Depends(get_db)]
