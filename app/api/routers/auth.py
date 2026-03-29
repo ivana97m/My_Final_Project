00beb5c7f9fe9cd7736d7d426a3faf3c33b89a11
ivana97m <ivsmilojevic@gmail.com>
2026-03-29 16:21:46 +0200

    register and login routes added

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
 
+from app.api.deps import DB
 from app.schemas.user import TokenResponse, UserLoginRequest, UserRegisterRequest, UserResponse
+from app.services.auth_service import auth_service
 
 router = APIRouter(tags=["auth"])
 
-@router.post("/auth", response_model=UserResponse,status_code=201, summary="Register new user")
-async def auth(request: Request):
+@router.post("/register", response_model=UserResponse,status_code=201, summary="Register new user")
+async def register (data: UserRegisterRequest, db: DB) -> UserResponse:
+    user = await auth_service.register(data, db)
+    return UserResponse.model_validate(user)
+
+@router.post("/login", response_model=TokenResponse, status_code=200, summary="Log In")
+async def login (data: UserLoginRequest, db: DB) -> TokenResponse:
+    return await auth_service.login(db, data.username, data.password)
 
